6aad0bd48cccafefe849e7156fb5d400a82938ba
AlihanAt <s0565573@htw-berlin.de>
2022-09-23 21:52:26 +0200

    Update AdvancedGameStateRatingFunction.java

--- src/Main/Logic/Rating/AdvancedGameStateRatingFunction.java
+++ src/Main/Logic/Rating/AdvancedGameStateRatingFunction.java
@@ -10,6 +10,10 @@ public class AdvancedGameStateRatingFunction extends RatingFunction {
     private final float fieldRatingMultiplier;
     private final float stonesOnLineMultiplier;
 
+    private final float playerWonEvaluation = 10000;
+    private final float playerLostEvaluation = -10000;
+    private final float noFreeFieldEvaluation = -100;
+
     public AdvancedGameStateRatingFunction(float ownPoints, float fieldRating, float stonesOnLine) {
         this.ownPointsMultiplier = ownPoints;
         this.fieldRatingMultiplier = fieldRating;
@@ -18,13 +22,17 @@ public class AdvancedGameStateRatingFunction extends RatingFunction {
 
     public float evaluateGame(Board board, int playerNo) {
         float evaluation = (ownPointsMultiplier * board.getPlayerScore(playerNo))
-                + (fieldRatingMultiplier * board.getFieldRatingForEnemies(playerNo))
+                - (fieldRatingMultiplier * board.getFieldRatingForEnemies(playerNo))
                 + (stonesOnLineMultiplier * board.getFreeFieldsOnBaseline(playerNo));
 
+        if(board.getFreeFieldsOnBaseline(playerNo) == 0){
+            evaluation += noFreeFieldEvaluation;
+        }
+
         if (board.hasPlayerWon(playerNo))
-            evaluation += 1000;
+            evaluation += playerWonEvaluation;
         else if (board.hasEnemyWon(playerNo))
-            evaluation *= -1;
+            evaluation += playerLostEvaluation;
 
         return evaluation;
     }
